61ed2e6ac459703fbcde655e66e13b632b6b2514
Muhammad Ahsan <itsahsan.do@gmail.com>
2026-02-02 18:40:02 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Extra test function
+void sayHello() {
+    cout << "Hello from Git test function!" << endl;
+}
+
+int main() {
+    cout << "Original main function" << endl;
+    
+    // Call the test function
+    sayHello();
+    
+    return 0;
+}
